5661edffd45c085a618b17a3df9755c105f23068
Novavisun <novavisun@gmail.com>
2026-03-04 13:28:29 -0300

    home abre calculadora

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 export default function Home() {
-return (
-<div style={{ height: "100vh", margin: 0 }}>
-<iframe
-src="/drops.html"
-style={{ width: "100%", height: "100%", border: "0" }}
-title="Novavisun Calculadora DROPS"
-/>
-</div>
-);
+  return (
+    <div style={{ height: "100vh", margin: 0 }}>
+      <iframe
+        src="/drops.html"
+        style={{ width: "100%", height: "100%", border: "0" }}
+        title="Novavisun Calculadora DROPS"
+      />
+    </div>
+  );
 }
